56706719e99ac312c59e9bc3db4ca39eb681a5ac
esterfeliciano <ester.felicianomelo@gmail.com>
2026-07-29 10:35:06 -0300

    Adiciona a entidade de tarefas (Task) mapeada no banco de dados com SQLAlchemy e o Enum de estados (TaskState), vinculando tudo ao usuário através de chave estrangeira.

---
+++ fast_zero/tasks/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from enum import Enum
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column
+from fast_zero.users.models import table_registry
+
+
+class TaskState(str, Enum):
+    draft = 'draft'
+    todo = 'todo'
+    doing = 'doing'
+    done = 'done'
+    trash = 'trash'
+
+
+@table_registry.mapped_as_dataclass
+class Task:
+    __tablename__ = 'tasks'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    state: Mapped[TaskState]
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
