838c5cb38bcd22a633e1aa3e517f2a9a4dcdfe42
esterfeliciano <ester.felicianomelo@gmail.com>
2026-07-30 11:27:26 -0300

    Dando uma  casa descente

--- frontend/lojinha/src/app/app.ts
+++ frontend/lojinha/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ProductList } from './product-list/product-list';
+import { TaskList } from './task-list/task-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductList, TaskList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('lojinha');
+  protected title = 'lojinha';
 }
