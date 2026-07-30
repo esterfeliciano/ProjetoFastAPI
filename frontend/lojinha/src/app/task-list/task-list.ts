29d63166ac7f5c740c1937559792d24227ab2ed3
esterfeliciano <ester.felicianomelo@gmail.com>
2026-07-30 11:08:32 -0300

    Compoente de tarefas

--- frontend/lojinha/src/app/task-list/task-list.ts
+++ frontend/lojinha/src/app/task-list/task-list.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
+type TaskState = 'draft' | 'todo' | 'doing' | 'done' | 'trash';
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  state: TaskState;
+}
+
 @Component({
   selector: 'app-task-list',
   imports: [],
   templateUrl: './task-list.html',
-  styleUrl: './task-list.css',
+  styleUrl: './task-list.css'
 })
-export class TaskList {}
+export class TaskList {
+  tasks: Task[] = [
+    { id: 1, title: 'Cadastrar produtos', description: 'Subir catálogo inicial', state: 'doing' },
+    { id: 2, title: 'Configurar CORS', description: 'Liberar acesso do Angular', state: 'todo' },
+    { id: 3, title: 'Criar componente de login', description: '', state: 'done' },
+  ];
+}
