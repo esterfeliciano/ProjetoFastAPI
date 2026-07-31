abe19f879f33d85d7e5388d6aa7b3cfc02417c68
esterfeliciano <ester.felicianomelo@gmail.com>
2026-07-31 14:37:51 -0300

    Corrige tratamento de token JWT expirado (era 500, agora é 401)

--- backend/src/users/security.py
+++ backend/src/users/security.py
@@ -4,7 +4,7 @@ from zoneinfo import ZoneInfo
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jwt import DecodeError, decode, encode
+from jwt import PyJWTError, decode, encode
 from pwdlib import PasswordHash
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -56,7 +56,7 @@ def get_current_user(
         if not subject_email:
             raise credentials_exception
 
-    except DecodeError:
+    except PyJWTError:
         raise credentials_exception
 
     user = session.scalar(
